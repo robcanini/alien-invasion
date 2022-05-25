64fcf6a1064abbbf253ad96850a513c2e7717866
Roberto Canini <cyberleonsama@gmail.com>
2022-05-25 23:11:18 +0200

    chore: add cli arguments enforcing

--- cmd/invade/main.go
+++ cmd/invade/main.go
@@ -8,12 +8,13 @@ import (
 )
 
 var (
-	filePath     = flag.String("filePath", "grid_data.db", "The grid data file")
-	aliensNumber = flag.Int("aliens", 5, "The number of aliens invading the planet")
+	filePath     = flag.String("filePath", "not_specified", "The grid data file")
+	aliensNumber = flag.Int("aliens", -1, "The number of aliens invading the planet")
 )
 
 func main() {
 	flag.Parse()
+	enforceCliArgs()
 
 	err := invasion.Run(invasion.Spec{
 		PlanetName:           "X",
@@ -24,6 +25,18 @@ func main() {
 
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func enforceCliArgs() {
+	if *filePath == "not_specified" {
+		fmt.Println("You must specify --filePath argument. Check examples/data.db for file sample")
+		os.Exit(1)
+	}
+
+	if *aliensNumber <= 0 {
+		fmt.Println("You must set --aliens argument with a valid number (from 1 to number of cities in grid)")
 		os.Exit(1)
 	}
 }
